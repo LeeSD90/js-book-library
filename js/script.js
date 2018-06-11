746e2015f234443f6b95ac4e4f91f50fd9791070
LeeSD90 <lee.doyle90@gmail.com>
2018-06-11 14:26:45 +0200

    Add Book Contstructor

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-$(document).ready(function() {
+function Book(title, author, pages, read) {
+	this.title = title
+	this.author = author
+	this.pages = pages
+	read = read
 
-});
-
+	this.info = function() {
+		return `${this.title} by ${author}, ${pages} pages, ${this.read ? "read" : "not read yet"}`;
+	}
+}
